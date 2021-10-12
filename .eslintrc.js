68a2c236da4b84db5c79a29250123960a180dddb
vamanuilov <vamanuilov@gmail.com>
2021-10-12 23:30:19 +0700

    feat: add eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true
+  },
+  extends: ['airbnb-typescript', 'prettier', 'plugin:jest/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint', 'jest', 'import'],
+  rules: {
+    "prettier/prettier": "warn",
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'warn',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx', '.ts', '.js'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
+  }
+}
