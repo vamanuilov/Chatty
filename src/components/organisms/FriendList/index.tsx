b5afba46d526ef9d5000803ae7d97fa867f34baf
vamanuilov <vamanuilov@gmail.com>
2021-10-22 23:18:39 +0700

    feat: add selected state to friend list

--- src/components/organisms/FriendList/index.tsx
+++ src/components/organisms/FriendList/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import EmptyFriendList from '../../molekules/EmptyFriendList'
 import Friend from '../../molekules/Friend'
 
@@ -9,6 +10,8 @@ export interface IFriends {
 }
 
 const FriendList = ({ friends }: { friends: IFriends[] }) => {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null)
+
   if (friends && friends.length === 0) {
     return <EmptyFriendList />
   }
@@ -16,14 +19,15 @@ const FriendList = ({ friends }: { friends: IFriends[] }) => {
   return (
     <>
       {friends.map((friend, index) => (
-        <Friend
-          key={`${friend.name + index}`}
-          name={friend.name}
-          additionalText={friend.lastMessage}
-          icon={friend.icon}
-          isLastMessageFromUser={friend.isLastMessageFromUser}
-          isSelected={index === 2}
-        />
+        <div key={`${friend.name + index}`} onClick={() => setSelectedIndex(index)}>
+          <Friend
+            name={friend.name}
+            additionalText={friend.lastMessage}
+            icon={friend.icon}
+            isLastMessageFromUser={friend.isLastMessageFromUser}
+            isSelected={index === selectedIndex}
+          />
+        </div>
       ))}
     </>
   )
