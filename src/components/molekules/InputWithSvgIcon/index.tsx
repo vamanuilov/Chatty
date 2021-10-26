9b4ede0d317fadcc6e745c81e36e72fd8d13d65b
vamanuilov <vamanuilov@gmail.com>
2021-10-27 00:14:12 +0700

    fix: add onchangehandler

--- src/components/molekules/InputWithSvgIcon/index.tsx
+++ src/components/molekules/InputWithSvgIcon/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import SvgIconLabel from '../../atoms/SvgIconLabel'
 
 import './styles.scss'
@@ -5,15 +6,22 @@ import './styles.scss'
 interface IInputWithSvgIcon {
   id: string
   type: 'file' | 'button'
-  onClickHandler?: () => void
+  onClickHandler?: (e: React.MouseEvent<HTMLInputElement>) => void
+  onChangeHandler?: (e: React.ChangeEvent<HTMLInputElement>) => void
   children: React.ReactNode
 }
 
-const InputWithSvgIcon: React.FC<IInputWithSvgIcon> = ({ children, id, type, onClickHandler }) => {
+const InputWithSvgIcon: React.FC<IInputWithSvgIcon> = ({ children, id, type, onClickHandler, onChangeHandler }) => {
   return (
     <div className="input-with-svg-icon">
       <SvgIconLabel htmlForId={id}>{children}</SvgIconLabel>
-      <input className="input-with-svg-icon__input_hidden" id={id} type={type} onClick={onClickHandler}></input>
+      <input
+        className="input-with-svg-icon__input_hidden"
+        id={id}
+        type={type}
+        onChange={onChangeHandler}
+        onClick={onClickHandler}
+      ></input>
     </div>
   )
 }
