9a391c498f4bd547175539b3ab6d891428478d02
vamanuilov <vamanuilov@gmail.com>
2021-10-28 00:31:54 +0700

    fix: friend list types

--- src/components/organisms/FriendList/index.tsx
+++ src/components/organisms/FriendList/index.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
 import EmptyFriendList from '../../molekules/EmptyFriendList'
 import Friend from '../../molekules/Friend'
 
-export interface IFriends {
-  name: string
-  icon: 'male' | 'female'
-  lastMessage: string
-  isLastMessageFromUser: boolean
+import { IFriends, ISelectedFriend } from '../../../interface/friends'
+
+interface IFriendList {
+  friends: IFriends[] | undefined
+  selectedFriend: ISelectedFriend | undefined
+  handleSelectFriend: (id: string) => void
 }
 
-const FriendList = ({ friends }: { friends: IFriends[] }) => {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null)
-
-  if (friends && friends.length === 0) {
+const FriendList: React.FC<IFriendList> = ({ selectedFriend, handleSelectFriend, friends }) => {
+  if (!friends || (friends && friends.length === 0)) {
     return <EmptyFriendList />
   }
 
   return (
     <>
-      {friends.map((friend, index) => (
-        <div key={`${friend.name + index}`} onClick={() => setSelectedIndex(index)}>
-          <Friend
-            name={friend.name}
-            lastMessage={friend.lastMessage}
-            icon={friend.icon}
-            isLastMessageFromUser={friend.isLastMessageFromUser}
-            isSelected={index === selectedIndex}
-          />
-        </div>
-      ))}
+      {friends &&
+        friends.map(({ name, icon, lastMessage, isLastMessageFromUser, id }) => (
+          <div key={`${name + id}`} onClick={() => handleSelectFriend(id)}>
+            <Friend
+              name={name}
+              lastMessage={lastMessage}
+              icon={icon}
+              isLastMessageFromUser={isLastMessageFromUser}
+              isSelected={id === selectedFriend?.id}
+            />
+          </div>
+        ))}
     </>
   )
 }
