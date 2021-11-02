511bfb607930c275a556279d4b1272f254b9685f
vamanuilov <vamanuilov@gmail.com>
2021-11-02 17:35:01 +0700

    fix: refs after review

--- src/components/atoms/InputField/index.tsx
+++ src/components/atoms/InputField/index.tsx
@@ -1,24 +1,22 @@
 import React from 'react'
-import { UseFormRegister } from 'react-hook-form'
-import { IFormInputs, IInput } from '../../../interface/input'
+// import { UseFormRegister } from 'react-hook-form'
+import { IInput } from '../../../interface/input'
 
 import './styles.scss'
 
-const TextInputAtom = React.forwardRef<HTMLInputElement, IInput & ReturnType<UseFormRegister<IFormInputs>>>(
-  ({ type, id, placeholder, isRequired, onBlur, onChange, name }, ref) => (
-    <input
-      className="input-field-atom"
-      id={id}
-      type={type}
-      placeholder={placeholder}
-      required={isRequired}
-      autoComplete="off"
-      ref={ref}
-      onBlur={onBlur}
-      onChange={onChange}
-      name={name}
-    />
-  )
+const TextInputAtom: React.FC<IInput> = ({ type, id, placeholder, isRequired, onBlur, onChange, name, innerRef }) => (
+  <input
+    className="input-field-atom"
+    id={id}
+    type={type}
+    placeholder={placeholder}
+    required={isRequired}
+    autoComplete="off"
+    ref={innerRef}
+    onBlur={onBlur}
+    onChange={onChange}
+    name={name}
+  />
 )
 
 export default TextInputAtom
--- src/interface/input.ts
+++ src/interface/input.ts
@@ -1,9 +1,14 @@
+import { ChangeHandler } from 'react-hook-form'
+
 export interface IInput {
   type: 'text' | 'login' | 'password'
   id: string
   placeholder: string
   isRequired?: boolean
   name?: string
+  onBlur?: ChangeHandler
+  onChange?: ChangeHandler
+  innerRef?: React.ForwardedRef<HTMLInputElement>
 }
 
 export interface IFormInputs {
