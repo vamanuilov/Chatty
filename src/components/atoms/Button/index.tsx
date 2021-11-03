a9598a8968f6cc24dc570fac258d557c600f9c84
vamanuilov <vamanuilov@gmail.com>
2021-11-04 02:04:11 +0700

    feat: add additional button styles

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import cn from 'classnames'
 
 import './styles.scss'
 
 type ButtonText = {
   buttonText: string
-  type?: 'submit'
+  type?: 'submit' | 'button'
   isDisabled?: boolean
   onClickHandler?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  isMainButton?: boolean
 }
 
-const Button: React.FC<ButtonText> = ({ type, buttonText, isDisabled, onClickHandler }) => {
+const Button: React.FC<ButtonText> = ({ type = 'button', buttonText, isDisabled, onClickHandler, isMainButton }) => {
   return (
-    <button className="button-atom" type={type || 'button'} disabled={isDisabled || false} onClick={onClickHandler}>
+    <button
+      className={cn('button-atom', { 'button-atom_main': isMainButton })}
+      type={type}
+      disabled={isDisabled || false}
+      onClick={onClickHandler}
+    >
       {buttonText}
     </button>
   )
