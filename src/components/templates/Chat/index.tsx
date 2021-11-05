ce3489f56dde87572e8effc51084fe914119e99b
vamanuilov <vamanuilov@gmail.com>
2021-11-06 05:42:51 +0700

    fix: add bg svg instead of img element

--- src/components/templates/Chat/index.tsx
+++ src/components/templates/Chat/index.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react-lite'
 import cn from 'classnames'
 
-import Image from '../../atoms/Image'
-import background from '../../../assets/images/background.png'
+import { ReactComponent as BackgroundImg } from '../../../assets/images/Background.svg'
 import ChatMessages from '../../organisms/ChatMessages'
 import Loader from '../../organisms/Loader'
 
@@ -13,15 +12,13 @@ import friendsStore from '../../../store/friends'
 import classNames from 'classnames'
 
 const Chat: React.FC = () => {
-  const backgroundSize = { height: 'auto', width: '1300px' }
-
   return (
     <div className={classNames('chat-container', { 'chat-container_hidden': !friendsStore.selectedFriend })}>
       <div className="chat-container__loader">
         <Loader isLoading={friendsStore.isLoading} />
       </div>
       <div className="chat-container__background">
-        <Image src={background} altText="background" size={backgroundSize} />
+        <BackgroundImg className="chat-container__img" />
       </div>
       <div className={cn('chat', { chat_loading: friendsStore.isLoading })}>
         <ChatMessages />
